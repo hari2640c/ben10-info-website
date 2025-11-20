04835ed1862ceb18f3e93f437cd04fc560e20049
Hari Narayanan.V <168257534+hari2640c@users.noreply.github.com>
2025-11-20 19:13:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,141 @@
+
+let watchselect=document.querySelector('.watch_click');
+let menuselect=document.querySelector('.menu');
+
+watchselect.onclick=function(){
+    watchselect.classList.toggle('active');
+    menuselect.classList.toggle('active');
+}
+
+
+
+
+//og1 alienimg1
+document.getElementById('og1').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg1').style.visibility='visible';
+    document.getElementById('alienogtext1').style.visibility='visible';
+})
+
+document.getElementById('og1').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg1').style.visibility='hidden';
+     document.getElementById('alienogtext1').style.visibility='hidden';
+})
+
+//og2 alienimg2
+document.getElementById('og2').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg2').style.visibility='visible';
+    document.getElementById('alienogtext2').style.visibility='visible';
+})
+
+document.getElementById('og2').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg2').style.visibility='hidden';
+     document.getElementById('alienogtext2').style.visibility='hidden';
+})
+//og3 alienimg3
+
+document.getElementById('og3').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg3').style.visibility='visible';
+    document.getElementById('alienogtext3').style.visibility='visible';
+})
+
+document.getElementById('og3').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg3').style.visibility='hidden';
+     document.getElementById('alienogtext3').style.visibility='hidden';
+})
+//og4 alienimg4
+
+document.getElementById('og4').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg4').style.visibility='visible';
+    document.getElementById('alienogtext4').style.visibility='visible';
+})
+
+document.getElementById('og4').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg4').style.visibility='hidden';
+     document.getElementById('alienogtext4').style.visibility='hidden';
+})
+
+//og5 alienimg5
+document.getElementById('og5').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg5').style.visibility='visible';
+    document.getElementById('alienogtext5').style.visibility='visible';
+})
+
+document.getElementById('og5').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg5').style.visibility='hidden';
+     document.getElementById('alienogtext5').style.visibility='hidden';
+})
+//og6 alienimg6
+document.getElementById('og6').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg6').style.visibility='visible';
+    document.getElementById('alienogtext6').style.visibility='visible';
+})
+
+document.getElementById('og6').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg6').style.visibility='hidden';
+     document.getElementById('alienogtext6').style.visibility='hidden';
+})
+
+//og7 alienimg7
+document.getElementById('og7').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg7').style.visibility='visible';
+    document.getElementById('alienogtext7').style.visibility='visible';
+})
+
+document.getElementById('og7').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg7').style.visibility='hidden';
+     document.getElementById('alienogtext7').style.visibility='hidden';
+})
+
+//og8 alienimg8
+document.getElementById('og8').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg8').style.visibility='visible';
+    document.getElementById('alienogtext8').style.visibility='visible';
+})
+
+document.getElementById('og8').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg8').style.visibility='hidden';
+     document.getElementById('alienogtext8').style.visibility='hidden';
+})
+
+//og9 alienimg9
+document.getElementById('og9').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg9').style.visibility='visible';
+    document.getElementById('alienogtext9').style.visibility='visible';
+})
+
+document.getElementById('og9').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg9').style.visibility='hidden';
+     document.getElementById('alienogtext9').style.visibility='hidden';
+})
+
+//og10 alienimg10
+document.getElementById('og10').addEventListener('mouseenter',()=>
+{
+    document.getElementById('alienogimg10').style.visibility='visible';
+    document.getElementById('alienogtext10').style.visibility='visible';
+})
+
+document.getElementById('og10').addEventListener('mouseleave',()=>
+{
+    document.getElementById('alienogimg10').style.visibility='hidden';
+     document.getElementById('alienogtext10').style.visibility='hidden';
+});
+
